40ddfa55d0bbca6183fe0a4c6347ee8f4c17ed26
TIAGO CAROLINO <6355267@formacao.iefp.pt>
2023-05-02 18:48:20 +0100

    FrontEndCreation

---
+++ ProjetoFinal/Interfaces/IWebServiceAPI.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoFinalAPI.Models;
+using Refit;
+
+namespace ProjetoFinal.Interfaces
+{
+    public interface IWebServiceAPI
+    {
+        [Get("/products")]
+        public Task<List<Product>> GetProducts();
+
+        [Get("/product-details/{id}")]
+        public Task<Product> GetProduct(int id);
+
+        [Post("/products")]
+        public Task<Product> CreateProduct([Body] Product newProduct);
+
+        [Put("/product-edit/{id}")]
+        public Task<Product> UpdateProduct([FromBody] Product updatedProduct, int id);
+
+
+        [Delete("/products/{id}")]
+        public Task DeleteProduct(int id);
+    }
+}
